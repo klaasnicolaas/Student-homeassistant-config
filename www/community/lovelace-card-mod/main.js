ff0fffa77172c9df966315ee7d83182ed2806a2d
Klaas Schoute <klaas_schoute@hotmail.com>
2020-03-19 15:05:36 +0100

    :truck: Clean shit in resources

--- www/community/lovelace-card-mod/main.js
+++
@@ -1,6 +0,0 @@
-import "./card-mod";
-import "./ha-card";
-import "./hui-entities-card";
-import "./hui-glance-card";
-import "./hui-state-label-badge";
-import "./mod-card"
--- www/community/lovelace-fold-entity-row/.github/feature-request.md
+++
@@ -1,7 +0,0 @@
----
-name: Feature request
-about: For suggesting new features
-title: ''
-labels: ''
-assignees: ''
----
