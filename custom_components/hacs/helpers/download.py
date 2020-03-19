5cd0b628522fc2cb0791246e9138c59594e8ad90
Klaas Schoute <klaas_schoute@hotmail.com>
2020-03-19 14:07:31 +0100

    ⬆️ Upgrade HACS to 0.23.2

--- custom_components/hacs/helpers/download.py
+++ custom_components/hacs/helpers/download.py
@@ -4,6 +4,94 @@ import tempfile
 import zipfile
 from custom_components.hacs.hacsbase.exceptions import HacsException
 from custom_components.hacs.handler.download import async_download_file, async_save_file
+from custom_components.hacs.helpers.filters import filter_content_return_one_of_type
+
+
+class FileInformation:
+    def __init__(self, url, path, name):
+        self.download_url = url
+        self.path = path
+        self.name = name
+
+
+def should_try_releases(repository):
+    """Return a boolean indicating whether to download releases or not."""
+    if repository.data.zip_release:
+        if repository.data.filename.endswith(".zip"):
+            if repository.ref != repository.data.default_branch:
+                return True
+    if repository.ref == repository.data.default_branch:
+        return False
+    if repository.data.category not in ["plugin", "theme"]:
+        return False
+    if not repository.releases.releases:
+        return False
+    return True
+
+
+def gather_files_to_download(repository):
+    """Return a list of file objects to be downloaded."""
+    files = []
+    tree = repository.tree
+    ref = f"{repository.ref}".replace("tags/", "")
+    releaseobjects = repository.releases.objects
+    category = repository.data.category
+    remotelocation = repository.content.path.remote
+
+    if should_try_releases(repository):
+        for release in releaseobjects or []:
+            if ref == release.tag_name:
+                for asset in release.assets or []:
+                    files.append(asset)
+        if files:
+            return files
+
+    if repository.content.single:
+        for treefile in tree:
+            if treefile.filename == repository.data.file_name:
+                files.append(
+                    FileInformation(
+                        treefile.download_url, treefile.full_path, treefile.filename
+                    )
+                )
+        return files
+
+    if category == "plugin":
+        for treefile in tree:
+            if treefile.path in ["", "dist"]:
+                if remotelocation == "dist" and not treefile.filename.startswith(
+                    "dist"
+                ):
+                    continue
+                if not remotelocation:
+                    if not treefile.filename.endswith(".js"):
+                        continue
+                    if treefile.path != "":
+                        continue
+                if not treefile.is_directory:
+                    files.append(
+                        FileInformation(
+                            treefile.download_url, treefile.full_path, treefile.filename
+                        )
+                    )
+        if files:
+            return files
+
+    if repository.data.content_in_root:
+        if not repository.data.filename:
+            if category == "theme":
+                tree = filter_content_return_one_of_type(
+                    repository.tree, "", "yaml", "full_path"
+                )
+
+    for path in tree:
+        if path.is_directory:
+            continue
+        if path.full_path.startswith(repository.content.path.remote):
+            files.append(
+                FileInformation(path.download_url, path.full_path, path.filename)
+            )
+    return files
 
 
 async def download_zip(repository, validate):
@@ -21,21 +109,17 @@ async def download_zip(repository, validate):
             return validate
 
         for content in contents:
-            filecontent = await async_download_file(
-                repository.hass, content.download_url
-            )
+            filecontent = await async_download_file(content.download_url)
 
             if filecontent is None:
                 validate.errors.append(f"[{content.name}] was not downloaded.")
                 continue
 
             result = await async_save_file(
-                f"{tempfile.gettempdir()}/{repository.repository_manifest.filename}",
-                filecontent,
+                f"{tempfile.gettempdir()}/{repository.data.filename}", filecontent
             )
             with zipfile.ZipFile(
-                f"{tempfile.gettempdir()}/{repository.repository_manifest.filename}",
-                "r",
+                f"{tempfile.gettempdir()}/{repository.data.filename}", "r"
             ) as zip_file:
                 zip_file.extractall(repository.content.path.local)
 
@@ -49,71 +133,49 @@ async def download_zip(repository, validate):
     return validate
 
 
-async def download_content(repository, validate, local_directory):
+async def download_content(repository):
     """Download the content of a directory."""
-    contents = []
-    try:
-        if repository.releases.releases and repository.information.category in [
-            "plugin",
-            "theme",
-        ]:
-            for release in repository.releases.objects:
-                if repository.status.selected_tag == release.tag_name:
-                    for asset in release.assets:
-                        contents.append(asset)
-        if not contents:
-            if repository.content.single:
-                for repository_object in repository.content.objects:
-                    contents.append(repository_object)
-            else:
-                tree = await repository.repository_object.get_tree(repository.ref)
-                for path in tree:
-                    if path.is_directory:
-                        continue
-                    if repository.content.path.remote in path.full_path:
-                        path.name = path.filename
-                        contents.append(path)
+    contents = gather_files_to_download(repository)
+    repository.logger.debug(repository.data.filename)
+    if not contents:
+        raise HacsException("No content to download")
 
-        if not contents:
-            raise HacsException("No content to download")
-
-        for content in contents:
-            repository.logger.debug(f"Downloading {content.name}")
-
-            filecontent = await async_download_file(
-                repository.hass, content.download_url
-            )
-
-            if filecontent is None:
-                validate.errors.append(f"[{content.name}] was not downloaded.")
+    for content in contents:
+        if repository.data.content_in_root and repository.data.filename:
+            if content.name != repository.data.filename:
                 continue
+        repository.logger.debug(f"Downloading {content.name}")
 
-            # Save the content of the file.
-            if repository.content.single or content.path is None:
-                local_directory = repository.content.path.local
+        filecontent = await async_download_file(content.download_url)
 
-            else:
-                _content_path = content.path
-                if not repository.repository_manifest.content_in_root:
-                    _content_path = _content_path.replace(
-                        f"{repository.content.path.remote}", ""
-                    )
+        if filecontent is None:
+            repository.validate.errors.append(f"[{content.name}] was not downloaded.")
+            continue
 
-                local_directory = f"{repository.content.path.local}/{_content_path}"
-                local_directory = local_directory.split("/")
-                del local_directory[-1]
-                local_directory = "/".join(local_directory)
+        # Save the content of the file.
+        if repository.content.single or content.path is None:
+            local_directory = repository.content.path.local
 
-            # Check local directory
-            pathlib.Path(local_directory).mkdir(parents=True, exist_ok=True)
+        else:
+            _content_path = content.path
+            if not repository.data.content_in_root:
+                _content_path = _content_path.replace(
+                    f"{repository.content.path.remote}", ""
+                )
 
-            local_file_path = f"{local_directory}/{content.name}"
-            result = await async_save_file(local_file_path, filecontent)
-            if result:
-                repository.logger.info(f"download of {content.name} complete")
-                continue
-            validate.errors.append(f"[{content.name}] was not downloaded.")
+            local_directory = f"{repository.content.path.local}/{_content_path}"
+            local_directory = local_directory.split("/")
+            del local_directory[-1]
+            local_directory = "/".join(local_directory)
 
-    except Exception as exception:  # pylint: disable=broad-except
-        validate.errors.append(f"Download was not complete [{exception}]")
-    return validate
+        # Check local directory
+        pathlib.Path(local_directory).mkdir(parents=True, exist_ok=True)
+
+        local_file_path = (f"{local_directory}/{content.name}").replace("//", "/")
+
+        result = await async_save_file(local_file_path, filecontent)
+        if result:
+            repository.logger.info(f"download of {content.name} complete")
+            continue
+        repository.validate.errors.append(f"[{content.name}] was not downloaded.")
+
