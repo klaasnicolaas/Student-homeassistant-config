a9fe3d9de4427639cb19a5b10d1ca1fea55b1220
Klaas Schoute <klaas_schoute@hotmail.com>
2022-11-14 00:19:50 +0100

    ⬆️ Update HACS integration to v1.28.3

--- custom_components/hacs/hacs_frontend_experimental/entrypoint.js
+++ custom_components/hacs/hacs_frontend_experimental/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-bd0f996b.js')")();
+  new Function("import('/hacsfiles/frontend/main-ec7846c8.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-bd0f996b.js';
+  el.src = '/hacsfiles/frontend/main-ec7846c8.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
