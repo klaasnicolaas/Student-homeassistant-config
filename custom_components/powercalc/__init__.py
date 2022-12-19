f6a5588bf9591fcc23a568759a768b6a680b7307
Klaas Schoute <klaas_schoute@hotmail.com>
2022-12-19 15:09:08 +0100

    ✨ Add powercalc custom integration

---
+++ custom_components/powercalc/__init__.py
@@ -0,0 +1,495 @@
+"""The PowerCalc integration."""
+
+from __future__ import annotations
+
+import logging
+import re
+from typing import Optional
+
+import homeassistant.helpers.config_validation as cv
+import homeassistant.helpers.entity_registry as er
+import voluptuous as vol
+from awesomeversion.awesomeversion import AwesomeVersion
+from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.components.utility_meter import DEFAULT_OFFSET, max_28_days
+from homeassistant.components.utility_meter.const import METER_TYPES
+from homeassistant.config_entries import (
+    SOURCE_INTEGRATION_DISCOVERY,
+    SOURCE_USER,
+    ConfigEntry,
+    ConfigEntryState,
+)
+from homeassistant.const import (
+    CONF_DOMAIN,
+    CONF_ENTITIES,
+    CONF_ENTITY_ID,
+    CONF_NAME,
+    CONF_PLATFORM,
+    CONF_SCAN_INTERVAL,
+    CONF_UNIQUE_ID,
+    EVENT_HOMEASSISTANT_STARTED,
+    Platform,
+)
+from homeassistant.const import __version__ as HA_VERSION
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import discovery, discovery_flow
+from homeassistant.helpers.typing import ConfigType
+
+from .aliases import MANUFACTURER_WLED
+from .common import SourceEntity, create_source_entity, validate_name_pattern
+from .const import (
+    CONF_CREATE_DOMAIN_GROUPS,
+    CONF_CREATE_ENERGY_SENSORS,
+    CONF_CREATE_UTILITY_METERS,
+    CONF_DISABLE_EXTENDED_ATTRIBUTES,
+    CONF_ENABLE_AUTODISCOVERY,
+    CONF_ENERGY_INTEGRATION_METHOD,
+    CONF_ENERGY_SENSOR_CATEGORY,
+    CONF_ENERGY_SENSOR_FRIENDLY_NAMING,
+    CONF_ENERGY_SENSOR_NAMING,
+    CONF_ENERGY_SENSOR_PRECISION,
+    CONF_ENERGY_SENSOR_UNIT_PREFIX,
+    CONF_FORCE_UPDATE_FREQUENCY,
+    CONF_IGNORE_UNAVAILABLE_STATE,
+    CONF_MANUFACTURER,
+    CONF_MODE,
+    CONF_MODEL,
+    CONF_POWER_SENSOR_CATEGORY,
+    CONF_POWER_SENSOR_FRIENDLY_NAMING,
+    CONF_POWER_SENSOR_NAMING,
+    CONF_POWER_SENSOR_PRECISION,
+    CONF_UTILITY_METER_OFFSET,
+    CONF_UTILITY_METER_TARIFFS,
+    CONF_UTILITY_METER_TYPES,
+    DATA_CALCULATOR_FACTORY,
+    DATA_CONFIGURED_ENTITIES,
+    DATA_DISCOVERED_ENTITIES,
+    DATA_DOMAIN_ENTITIES,
+    DATA_USED_UNIQUE_IDS,
+    DEFAULT_ENERGY_INTEGRATION_METHOD,
+    DEFAULT_ENERGY_NAME_PATTERN,
+    DEFAULT_ENERGY_SENSOR_PRECISION,
+    DEFAULT_ENTITY_CATEGORY,
+    DEFAULT_POWER_NAME_PATTERN,
+    DEFAULT_POWER_SENSOR_PRECISION,
+    DEFAULT_UPDATE_FREQUENCY,
+    DEFAULT_UTILITY_METER_TYPES,
+    DISCOVERY_POWER_PROFILE,
+    DISCOVERY_SOURCE_ENTITY,
+    DISCOVERY_TYPE,
+    DOMAIN,
+    DOMAIN_CONFIG,
+    ENERGY_INTEGRATION_METHODS,
+    ENTITY_CATEGORIES,
+    MIN_HA_VERSION,
+    CalculationStrategy,
+    PowercalcDiscoveryType,
+    UnitPrefix,
+)
+from .errors import ModelNotSupported
+from .power_profile.model_discovery import (
+    PowerProfile,
+    autodiscover_model,
+    get_power_profile,
+)
+from .power_profile.power_profile import DEVICE_DOMAINS
+from .sensors.group import remove_from_associated_group_entries
+from .strategy.factory import PowerCalculatorStrategyFactory
+
+PLATFORMS = [Platform.SENSOR]
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.All(
+            cv.deprecated(
+                CONF_SCAN_INTERVAL, replacement_key=CONF_FORCE_UPDATE_FREQUENCY
+            ),
+            vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_FORCE_UPDATE_FREQUENCY, default=DEFAULT_UPDATE_FREQUENCY
+                    ): cv.time_period,
+                    vol.Optional(
+                        CONF_POWER_SENSOR_NAMING, default=DEFAULT_POWER_NAME_PATTERN
+                    ): validate_name_pattern,
+                    vol.Optional(
+                        CONF_POWER_SENSOR_FRIENDLY_NAMING
+                    ): validate_name_pattern,
+                    vol.Optional(
+                        CONF_POWER_SENSOR_CATEGORY, default=DEFAULT_ENTITY_CATEGORY
+                    ): vol.In(ENTITY_CATEGORIES),
+                    vol.Optional(
+                        CONF_ENERGY_SENSOR_NAMING, default=DEFAULT_ENERGY_NAME_PATTERN
+                    ): validate_name_pattern,
+                    vol.Optional(
+                        CONF_ENERGY_SENSOR_FRIENDLY_NAMING
+                    ): validate_name_pattern,
+                    vol.Optional(
+                        CONF_ENERGY_SENSOR_CATEGORY, default=DEFAULT_ENTITY_CATEGORY
+                    ): vol.In(ENTITY_CATEGORIES),
+                    vol.Optional(
+                        CONF_DISABLE_EXTENDED_ATTRIBUTES, default=False
+                    ): cv.boolean,
+                    vol.Optional(CONF_ENABLE_AUTODISCOVERY, default=True): cv.boolean,
+                    vol.Optional(CONF_CREATE_ENERGY_SENSORS, default=True): cv.boolean,
+                    vol.Optional(CONF_CREATE_UTILITY_METERS, default=False): cv.boolean,
+                    vol.Optional(CONF_UTILITY_METER_TARIFFS, default=[]): vol.All(
+                        cv.ensure_list, [cv.string]
+                    ),
+                    vol.Optional(
+                        CONF_UTILITY_METER_TYPES, default=DEFAULT_UTILITY_METER_TYPES
+                    ): vol.All(cv.ensure_list, [vol.In(METER_TYPES)]),
+                    vol.Optional(
+                        CONF_UTILITY_METER_OFFSET, default=DEFAULT_OFFSET
+                    ): vol.All(cv.time_period, cv.positive_timedelta, max_28_days),
+                    vol.Optional(
+                        CONF_ENERGY_INTEGRATION_METHOD,
+                        default=DEFAULT_ENERGY_INTEGRATION_METHOD,
+                    ): vol.In(ENERGY_INTEGRATION_METHODS),
+                    vol.Optional(
+                        CONF_ENERGY_SENSOR_PRECISION,
+                        default=DEFAULT_ENERGY_SENSOR_PRECISION,
+                    ): cv.positive_int,
+                    vol.Optional(
+                        CONF_POWER_SENSOR_PRECISION,
+                        default=DEFAULT_POWER_SENSOR_PRECISION,
+                    ): cv.positive_int,
+                    vol.Optional(
+                        CONF_ENERGY_SENSOR_UNIT_PREFIX, default=UnitPrefix.KILO
+                    ): vol.In([cls.value for cls in UnitPrefix]),
+                    vol.Optional(CONF_CREATE_DOMAIN_GROUPS, default=[]): vol.All(
+                        cv.ensure_list, [cv.string]
+                    ),
+                    vol.Optional(CONF_IGNORE_UNAVAILABLE_STATE): cv.boolean,
+                }
+            ),
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    if AwesomeVersion(HA_VERSION) < AwesomeVersion(MIN_HA_VERSION):
+        _LOGGER.critical(
+            "Your HA version is outdated for this version of powercalc. Minimum required HA version is %s",
+            MIN_HA_VERSION,
+        )
+        return False
+
+    domain_config = config.get(DOMAIN) or {
+        CONF_POWER_SENSOR_NAMING: DEFAULT_POWER_NAME_PATTERN,
+        CONF_POWER_SENSOR_PRECISION: DEFAULT_POWER_SENSOR_PRECISION,
+        CONF_POWER_SENSOR_CATEGORY: DEFAULT_ENTITY_CATEGORY,
+        CONF_ENERGY_INTEGRATION_METHOD: DEFAULT_ENERGY_INTEGRATION_METHOD,
+        CONF_ENERGY_SENSOR_NAMING: DEFAULT_ENERGY_NAME_PATTERN,
+        CONF_ENERGY_SENSOR_PRECISION: DEFAULT_ENERGY_SENSOR_PRECISION,
+        CONF_ENERGY_SENSOR_CATEGORY: DEFAULT_ENTITY_CATEGORY,
+        CONF_ENERGY_SENSOR_UNIT_PREFIX: UnitPrefix.KILO,
+        CONF_FORCE_UPDATE_FREQUENCY: DEFAULT_UPDATE_FREQUENCY,
+        CONF_DISABLE_EXTENDED_ATTRIBUTES: False,
+        CONF_IGNORE_UNAVAILABLE_STATE: False,
+        CONF_CREATE_DOMAIN_GROUPS: [],
+        CONF_CREATE_ENERGY_SENSORS: True,
+        CONF_CREATE_UTILITY_METERS: False,
+        CONF_ENABLE_AUTODISCOVERY: True,
+        CONF_UTILITY_METER_OFFSET: DEFAULT_OFFSET,
+        CONF_UTILITY_METER_TYPES: DEFAULT_UTILITY_METER_TYPES,
+    }
+
+    hass.data[DOMAIN] = {
+        DATA_CALCULATOR_FACTORY: PowerCalculatorStrategyFactory(hass),
+        DOMAIN_CONFIG: domain_config,
+        DATA_CONFIGURED_ENTITIES: {},
+        DATA_DOMAIN_ENTITIES: {},
+        DATA_DISCOVERED_ENTITIES: {},
+        DATA_USED_UNIQUE_IDS: [],
+    }
+
+    if domain_config.get(CONF_ENABLE_AUTODISCOVERY):
+        discovery_manager = DiscoveryManager(hass, config)
+        await discovery_manager.start_discovery()
+
+    if domain_config.get(CONF_CREATE_DOMAIN_GROUPS):
+
+        async def _create_domain_groups(event: None):
+            await create_domain_groups(
+                hass,
+                domain_config,
+                domain_config.get(CONF_CREATE_DOMAIN_GROUPS),
+            )
+
+        hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_STARTED,
+            _create_domain_groups,
+        )
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Powercalc integration from a config entry."""
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_update_entry))
+    return True
+
+
+async def async_update_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Update a given config entry."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        config_entry, PLATFORMS
+    )
+
+    if unload_ok:
+        used_unique_ids: list[str] = hass.data[DOMAIN][DATA_USED_UNIQUE_IDS]
+        try:
+            used_unique_ids.remove(config_entry.unique_id)
+        except ValueError:
+            return True
+
+    return unload_ok
+
+
+async def async_remove_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Called after a config entry is removed."""
+    updated_entries = await remove_from_associated_group_entries(hass, config_entry)
+    for group_entry in updated_entries:
+        if group_entry.state == ConfigEntryState.LOADED:
+            await hass.config_entries.async_reload(group_entry.entry_id)
+
+
+async def create_domain_groups(
+    hass: HomeAssistant, global_config: dict, domains: list[str]
+):
+    """Create group sensors aggregating all power sensors from given domains"""
+    _LOGGER.debug("Setting up domain based group sensors..")
+    for domain in domains:
+        if domain not in hass.data[DOMAIN].get(DATA_DOMAIN_ENTITIES):
+            _LOGGER.error(f"Cannot setup group for domain {domain}, no entities found")
+            continue
+
+        domain_entities = hass.data[DOMAIN].get(DATA_DOMAIN_ENTITIES)[domain]
+
+        hass.async_create_task(
+            discovery.async_load_platform(
+                hass,
+                SENSOR_DOMAIN,
+                DOMAIN,
+                {
+                    DISCOVERY_TYPE: PowercalcDiscoveryType.DOMAIN_GROUP,
+                    CONF_ENTITIES: domain_entities,
+                    CONF_DOMAIN: domain,
+                },
+                global_config,
+            )
+        )
+
+
+class DiscoveryManager:
+    """
+    This class is responsible for scanning the HA instance for entities and their manufacturer / model info
+    It checks if any of these devices is supported in the powercalc library
+    When entities are found it will dispatch a discovery flow, so the user can add them to their HA instance
+    """
+
+    def __init__(self, hass: HomeAssistant, ha_config: ConfigType):
+        self.hass = hass
+        self.ha_config = ha_config
+        self.manually_configured_entities: list[str] | None = None
+
+    async def start_discovery(self):
+        """Start the discovery procedure"""
+
+        _LOGGER.debug("Start auto discovering entities")
+        entity_registry = er.async_get(self.hass)
+        for entity_entry in list(entity_registry.entities.values()):
+            if entity_entry.disabled:
+                continue
+
+            if entity_entry.domain not in DEVICE_DOMAINS.values():
+                continue
+
+            has_user_config = self._is_user_configured(entity_entry.entity_id)
+            if has_user_config:
+                _LOGGER.debug(
+                    "%s: Entity is manually configured, skipping auto configuration",
+                    entity_entry.entity_id,
+                )
+                continue
+
+            model_info = await autodiscover_model(self.hass, entity_entry)
+            if not model_info:
+                continue
+
+            source_entity = await create_source_entity(
+                entity_entry.entity_id, self.hass
+            )
+
+            if (
+                model_info.manufacturer == MANUFACTURER_WLED
+                and entity_entry.domain == LIGHT_DOMAIN
+                and not re.search(
+                    "master|segment",
+                    str(entity_entry.original_name),
+                    flags=re.IGNORECASE,
+                )
+            ):
+                self._init_entity_discovery(
+                    source_entity,
+                    power_profile=None,
+                    extra_discovery_data={
+                        CONF_MODE: CalculationStrategy.WLED,
+                        CONF_MANUFACTURER: model_info.manufacturer,
+                        CONF_MODEL: model_info.model,
+                    },
+                )
+                continue
+
+            try:
+                power_profile = await get_power_profile(
+                    self.hass, {}, model_info=model_info
+                )
+                if not power_profile:
+                    continue
+            except ModelNotSupported:
+                _LOGGER.debug(
+                    "%s: Model not found in library, skipping discovery",
+                    entity_entry.entity_id,
+                )
+                continue
+
+            if not power_profile.is_entity_domain_supported(source_entity.domain):
+                continue
+
+            self._init_entity_discovery(source_entity, power_profile, {})
+
+        _LOGGER.debug("Done auto discovering entities")
+
+    @callback
+    def _init_entity_discovery(
+        self,
+        source_entity: SourceEntity,
+        power_profile: PowerProfile | None,
+        extra_discovery_data: Optional[dict],
+    ):
+        """Dispatch the discovery flow for a given entity"""
+        existing_entries = [
+            entry
+            for entry in self.hass.config_entries.async_entries(DOMAIN)
+            if entry.unique_id == source_entity.unique_id
+        ]
+        if existing_entries:
+            _LOGGER.debug(
+                f"{source_entity.entity_id}: Already setup with discovery, skipping new discovery"
+            )
+            return
+
+        discovery_data = {
+            CONF_UNIQUE_ID: source_entity.unique_id,
+            CONF_NAME: source_entity.name,
+            CONF_ENTITY_ID: source_entity.entity_id,
+            DISCOVERY_SOURCE_ENTITY: source_entity,
+        }
+
+        if power_profile:
+            discovery_data[DISCOVERY_POWER_PROFILE] = power_profile
+            discovery_data[CONF_MANUFACTURER] = power_profile.manufacturer
+            discovery_data[CONF_MODEL] = power_profile.model
+
+        if extra_discovery_data:
+            discovery_data.update(extra_discovery_data)
+
+        discovery_flow.async_create_flow(
+            self.hass,
+            DOMAIN,
+            context={"source": SOURCE_INTEGRATION_DISCOVERY},
+            data=discovery_data,
+        )
+
+        # Code below if for legacy discovery routine, will be removed somewhere in the future
+        if power_profile and not power_profile.is_additional_configuration_required:
+            discovery_info = {
+                CONF_ENTITY_ID: source_entity.entity_id,
+                DISCOVERY_SOURCE_ENTITY: source_entity,
+                DISCOVERY_POWER_PROFILE: power_profile,
+                DISCOVERY_TYPE: PowercalcDiscoveryType.LIBRARY,
+            }
+            self.hass.async_create_task(
+                discovery.async_load_platform(
+                    self.hass, SENSOR_DOMAIN, DOMAIN, discovery_info, self.ha_config
+                )
+            )
+
+    def _is_user_configured(self, entity_id: str) -> bool:
+        """
+        Check if user have setup powercalc sensors for a given entity_id.
+        Either with the YAML or GUI method.
+        """
+        if not self.manually_configured_entities:
+            self.manually_configured_entities = (
+                self._load_manually_configured_entities()
+            )
+
+        return entity_id in self.manually_configured_entities
+
+    def _load_manually_configured_entities(self) -> list[str]:
+        """Looks at the YAML and GUI config entries for all the configured entity_id's"""
+        entities = []
+
+        # Find entity ids in yaml config
+        if SENSOR_DOMAIN in self.ha_config:
+            sensor_config = self.ha_config.get(SENSOR_DOMAIN)
+            platform_entries = [
+                item
+                for item in sensor_config
+                if isinstance(item, dict) and item.get(CONF_PLATFORM) == DOMAIN
+            ]
+            for entry in platform_entries:
+                entities.extend(self._find_entity_ids_in_yaml_config(entry))
+
+        # Add entities from existing config entries
+        entities.extend(
+            [
+                entry.data.get(CONF_ENTITY_ID)
+                for entry in self.hass.config_entries.async_entries(DOMAIN)
+                if entry.source == SOURCE_USER
+            ]
+        )
+
+        return entities
+
+    def _find_entity_ids_in_yaml_config(self, search_dict: dict):
+        """
+        Takes a dict with nested lists and dicts,
+        and searches all dicts for a key of the field
+        provided.
+        """
+        found_entity_ids = []
+
+        for key, value in search_dict.items():
+
+            if key == "entity_id":
+                found_entity_ids.append(value)
+
+            elif isinstance(value, dict):
+                results = self._find_entity_ids_in_yaml_config(value)
+                for result in results:
+                    found_entity_ids.append(result)
+
+            elif isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict):
+                        results = self._find_entity_ids_in_yaml_config(item)
+                        for result in results:
+                            found_entity_ids.append(result)
+
+        return found_entity_ids
---
+++ custom_components/powercalc/power_profile/library.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+import json
+import logging
+import os
+from typing import NamedTuple
+
+from homeassistant.core import HomeAssistant
+
+from ..aliases import MANUFACTURER_DIRECTORY_MAPPING
+from ..const import DATA_PROFILE_LIBRARY, DOMAIN
+from .power_profile import PowerProfile
+
+CUSTOM_DATA_DIRECTORY = "powercalc-custom-models"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ProfileLibrary:
+    def __init__(self, hass: HomeAssistant, extra_data_directories: list = None):
+        self._hass = hass
+        self._data_directories: list[str] = [
+            d
+            for d in (
+                os.path.join(hass.config.config_dir, CUSTOM_DATA_DIRECTORY),
+                os.path.join(os.path.dirname(__file__), "../custom_data"),
+                os.path.join(os.path.dirname(__file__), "../data"),
+            )
+            if os.path.exists(d)
+        ]
+        if extra_data_directories:
+            self._data_directories.extend(extra_data_directories)
+        self._profiles: dict[str, list[PowerProfile]] = {}
+
+    def factory(hass: HomeAssistant) -> ProfileLibrary:
+        """
+        Creates and loads the profile library
+        Makes sure it is only loaded once and instance is saved in hass data registry
+        """
+        if DOMAIN not in hass.data:
+            hass.data[DOMAIN] = {}
+
+        if DATA_PROFILE_LIBRARY in hass.data[DOMAIN]:
+            return hass.data[DOMAIN][DATA_PROFILE_LIBRARY]
+
+        library = ProfileLibrary(hass)
+        hass.data[DOMAIN][DATA_PROFILE_LIBRARY] = library
+        return library
+
+    factory = staticmethod(factory)
+
+    def get_manufacturer_listing(self) -> list[str]:
+        """Get listing of available manufacturers"""
+        manufacturers: list[str] = []
+        for data_dir in self._data_directories:
+            manufacturers.extend(next(os.walk(data_dir))[1])
+        return sorted(manufacturers)
+
+    def get_model_listing(self, manufacturer: str) -> list[str]:
+        """Get listing of available models for a given manufacturer"""
+        models: list[str] = []
+        for data_dir in self._data_directories:
+            manufacturer_dir = os.path.join(data_dir, manufacturer)
+            if not os.path.exists(manufacturer_dir):
+                continue
+            models.extend(os.listdir(manufacturer_dir))
+        return sorted(models)
+
+    async def get_profile(
+        self, model_info: ModelInfo, custom_directory: str | None = None
+    ) -> PowerProfile | None:
+        """Get a power profile for a given manufacturer and model"""
+        if custom_directory:
+            return await self._create_power_profile(model_info, custom_directory)
+
+        # Support multiple LUT in subdirectories
+        sub_profile = None
+        if "/" in model_info.model:
+            (model, sub_profile) = model_info.model.split("/", 1)
+            model_info = ModelInfo(model_info.manufacturer, model)
+
+        profiles = await self.get_profiles_by_manufacturer(model_info.manufacturer)
+        for profile in profiles:
+            if profile.supports(model_info.model):
+                if sub_profile:
+                    profile.select_sub_profile(sub_profile)
+                return profile
+
+        return None
+
+    async def get_profiles_by_manufacturer(
+        self, manufacturer: str
+    ) -> list[PowerProfile]:
+        """
+        Lazy loads a list of power profiles per manufacturer
+
+        Using the following lookup fallback mechanism:
+         - check in user defined directory (config/powercalc-custom-models)
+         - check in alternative user defined directory (config/custom_components/powercalc/custom_data)
+         - check in built-in directory (config/custom_components/powercalc/data)
+        """
+
+        if manufacturer in MANUFACTURER_DIRECTORY_MAPPING:
+            manufacturer = MANUFACTURER_DIRECTORY_MAPPING.get(manufacturer)
+        manufacturer = manufacturer.lower()
+
+        if manufacturer in self._profiles:
+            return self._profiles[manufacturer]
+
+        profiles = []
+        for data_dir in self._data_directories:
+            manufacturer_dir = os.path.join(data_dir, manufacturer)
+            if not os.path.exists(manufacturer_dir):
+                continue
+            for model in next(os.walk(manufacturer_dir))[1]:
+                if model[0] in [".", "@"]:
+                    continue
+                power_profile = await self._create_power_profile(
+                    ModelInfo(manufacturer, model),
+                    os.path.join(manufacturer_dir, model),
+                )
+                if power_profile is None:
+                    continue
+
+                profiles.append(power_profile)
+
+        self._profiles[manufacturer] = profiles
+        return profiles
+
+    async def _create_power_profile(
+        self, model_info: ModelInfo, directory: str
+    ) -> PowerProfile | None:
+        model_json_path = os.path.join(directory, "model.json")
+        try:
+            with open(model_json_path) as file:
+                json_data = json.load(file)
+                profile = PowerProfile(
+                    self._hass,
+                    manufacturer=model_info.manufacturer,
+                    model=model_info.model,
+                    directory=directory,
+                    json_data=json_data,
+                )
+                # When the power profile supplies multiple sub profiles we select one by default
+                if not profile.sub_profile and profile.sub_profile_select:
+                    profile.select_sub_profile(profile.sub_profile_select.default)
+
+        except FileNotFoundError:
+            _LOGGER.error("model.json file not found in directory %s", directory)
+            return None
+
+        return profile
+
+
+class ModelInfo(NamedTuple):
+    manufacturer: str
+    model: str
