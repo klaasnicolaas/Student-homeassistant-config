a99bbb40a11aacb1ba4cf718ad1addc19650c1e5
Klaas Schoute <klaas_schoute@hotmail.com>
2024-02-12 20:41:37 +0100

    ✨ Add Spook custom integration

---
+++ custom_components/spook/entity.py
@@ -0,0 +1,27 @@
+"""Spook - Not your homie."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from homeassistant.helpers.entity import Entity, EntityDescription
+
+
+@dataclass
+class SpookEntityDescription(EntityDescription):
+    """Defines an base Spook entity description."""
+
+    entity_id: str | None = None
+
+
+class SpookEntity(Entity):
+    """Defines an base Spook entity."""
+
+    entity_description: SpookEntityDescription
+
+    _attr_has_entity_name = True
+
+    def __init__(self, description: SpookEntityDescription) -> None:
+        """Initialize the entity."""
+        self.entity_description = description
+        if description.entity_id:
+            self.entity_id = description.entity_id
