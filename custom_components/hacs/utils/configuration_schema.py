330f798046d6cf2100040252ec2402fd5676fa59
Klaas Schoute <klaas_schoute@hotmail.com>
2024-11-21 02:28:44 +0100

    Remove all custom component integrations

--- custom_components/hacs/utils/configuration_schema.py
+++
@@ -1,9 +0,0 @@
-"""HACS Configuration Schemas."""
-
-# Configuration:
-SIDEPANEL_TITLE = "sidepanel_title"
-SIDEPANEL_ICON = "sidepanel_icon"
-APPDAEMON = "appdaemon"
-
-# Options:
-COUNTRY = "country"
--- custom_components/hacs/utils/json.py
+++
@@ -1,5 +0,0 @@
-"""JSON utils."""
-
-from homeassistant.util.json import json_loads
-
-__all__ = ["json_loads"]
